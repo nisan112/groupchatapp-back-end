e55e18e88609e57a3afcf5a0f44b9b4e2f407727
Nishan <nisanstha355@gmail.com>
2024-12-28 22:23:22 -0500

    new features added, RESTAPI, services,DAO,Removed interceptor

--- src/main/java/com/websocket/chyatt/Models/User.java
+++ src/main/java/com/websocket/chyatt/Models/User.java
@@ -1,7 +1,6 @@
 package com.websocket.chyatt.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
